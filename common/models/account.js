7ee26e809259c2b656ce367917cd1e62a1f85f07
Tien, Le Huu <lehuutien1996@gmail.com>
2017-05-07 00:18:29 +0700

    Hide role_name field in Account model

--- common/models/account.js
+++ common/models/account.js
@@ -1,5 +1,32 @@
 'use strict';
 
-module.exports = function(Account) {
+module.exports = function (Account) {
+
+  var app = require('../../server/server');
+
+  Account.observe('after save', function (context, next) {
+
+    if (context.isNewInstance) {
+
+      var Role        = app.models.Role;
+      var RoleMapping = app.models.RoleMapping;
+      var profileInfo = context.instance;
+
+      if (["admin", "user"].indexOf(profileInfo.role_name) >= 0) {
+
+        Role.findOne({ where: { name: profileInfo.role_name }}, function (error, role) {
+          role.principals.create({
+            principalType: RoleMapping.USER,
+            principalId: profileInfo.id
+          });
+        });
+
+      }
+
+    }
+
+    next();
+
+  });
 
 };
